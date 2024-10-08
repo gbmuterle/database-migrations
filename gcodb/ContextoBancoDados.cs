59bae4a2522dbc1247445e3a2eecefcf35a833af
leopellizzoni <lpellizzoni@ucs.br>
2024-10-08 17:56:24 -0300

    feat: database migrations project

---
+++ gcodb/ContextoBancoDados.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gcodb
+{
+    public class ContextoBancoDados : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder
+                .UseSqlServer("Server=localhost;Database=Teste;User Id=sa;Password=Senha;TrustServerCertificate=true");
+        }
+    }
+}
---
+++ gcodb/Program.cs
@@ -0,0 +1,3 @@
+﻿
+Console.WriteLine("Hello, GCO");
+
