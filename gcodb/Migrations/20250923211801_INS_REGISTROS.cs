0437e2f8ef02221c8d13c2f4ac7160def85345f0
leopellizzoni <lpellizzoni@ucs.br>
2025-09-23 18:21:35 -0300

    feat: exemplo adicionando registros na tabela

---
+++ gcodb/Migrations/20250923211801_INS_REGISTROS.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gcodb.Migrations
+{
+    /// <inheritdoc />
+    public partial class INS_REGISTROS : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO TABELATESTE (ID, DESCRICAO, DATAHORA) VALUES (1, 'Registro1', GETDATE())");
+            migrationBuilder.Sql("INSERT INTO TABELATESTE (ID, DESCRICAO, DATAHORA) VALUES (2, 'Registro2', GETDATE())");
+            migrationBuilder.Sql("INSERT INTO TABELATESTE (ID, DESCRICAO, DATAHORA) VALUES (3, 'Registro3', GETDATE())");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM TABELATESTE WHERE ID IN (1, 2, 3)");
+        }
+    }
+}
