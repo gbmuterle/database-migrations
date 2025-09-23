c35286c3270dbcadd7916e9dc8492605730c326a
leopellizzoni <lpellizzoni@ucs.br>
2025-09-23 17:08:45 -0300

    feat: migration para tabela de teste

---
+++ gcodb/Migrations/20250923195704_TABELA_TESTE.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gcodb.Migrations
+{
+    /// <inheritdoc />
+    public partial class TABELA_TESTE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE TABLE TABELATESTE( ID INT IDENTITY, DESCRICAO VARCHAR(100), DATAHORA DATETIME)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TABLE TABELATESTE");
+        }
+    }
+}
