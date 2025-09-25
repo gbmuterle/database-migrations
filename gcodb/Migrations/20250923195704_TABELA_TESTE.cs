4a1399685523dda0eac840850b011d68fe16004e
gbmuterle <gbmuterle@ucs.br>
2025-09-24 21:37:27 -0300

    Update 20250923195704_TABELA_TESTE.cs

--- gcodb/Migrations/20250923195704_TABELA_TESTE.cs
+++ gcodb/Migrations/20250923195704_TABELA_TESTE.cs
@@ -7,13 +7,16 @@ namespace gcodb.Migrations
     /// <inheritdoc />
     public partial class TABELA_TESTE : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("CREATE TABLE TABELATESTE( ID INT PRIMARY KEY, DESCRICAO VARCHAR(100), DATAHORA DATETIME)");
+            migrationBuilder.Sql(@"
+                CREATE TABLE TABELATESTE (
+                    ID INT PRIMARY KEY,
+                    DESCRICAO VARCHAR(100),
+                    DATAHORA DATETIME
+                )");
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql("DROP TABLE TABELATESTE");
